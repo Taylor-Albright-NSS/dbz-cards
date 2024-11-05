3903a23d442b06baa350b9447f0f16c27de6ae69
Taylor Albright <jtayloralbright@gmail.com>
2024-11-05 14:22:23 -0600

    Initial commit after renaming folder and setting up webapi

---
+++ Models/Card.cs
@@ -0,0 +1,10 @@
+namespace DbzCards.Models;
+
+public class Card 
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int PowerLevel { get; set; }
+    public string Race { get; set; }
+}
+
---
+++ Models/DTOs/CardDTO.cs
@@ -0,0 +1,9 @@
+namespace DbzCards.Models.DTOs;
+
+public class CardDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int PowerLevel { get; set; }
+    public string Race { get; set; }
+}
